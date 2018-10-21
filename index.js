2d406cd57324338b9ddeb022b1e078470e6c05f4
Mohammed Ajmal Siddiqui <ajmalsiddiqui21@gmail.com>
2018-10-20 20:46:22 -0400

    added route to use script

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const { execFile } = require('child_process');
+
 const app = express();
 
 const port = process.env.PORT || 8080;
@@ -9,7 +11,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/ci', (req, res) => {
-	res.send('hello');
+	try {
+		const child = execFile('node', ['--version'], (error, stdout, stderr) => {
+			if (error) {
+				throw error;
+			}
+			console.log(stdout);
+			res.status(200).json({
+				'message': 'successfully updated deployment'
+			});
+		});
+	} catch(e) {
+		console.log(e);
+		res.status(200).json({
+			'message': 'Something went wrong',
+			'error': JSON.stringify(e)
+		});
+	}
+	
 });
 
 app.listen(port, () => {
